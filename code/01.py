7e9842be96989196b82c68e4d2d266e82e3b0d8a
Elliott Rosenberg <elliott_rosenberg+cs1300@brown.edu>
2022-12-01 14:30:10 -0500

    first day

--- code/01.py
+++ code/01.py
@@ -1,6 +1,9 @@
 lines = [l.strip() for l in open("input/01/input1.txt").readlines()]
+
+#part 1
 maxCals = 0
 tempTotal = 0
+
 for line in lines:
     if line == '':
         if tempTotal > maxCals:
@@ -10,6 +13,24 @@ for line in lines:
         line = int(line)
         tempTotal += line
 print(maxCals)
+
+#part2
+
+calsList = []
+for line in lines:
+    if line == '':
+        calsList.append(tempTotal)
+        tempTotal = 0
+    else: 
+        line = int(line)
+        tempTotal += line
+
+calsList = sorted(calsList, reverse=True)
+print(calsList)
+
+print(calsList[1] + calsList[2] + calsList[3])
+
+
 
     
 
