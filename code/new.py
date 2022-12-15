2f3675c6c079db67d5ad494ac76d1293a970e5ac
elliott-rosenberg <elliott_rosenberg@brown.edu>
2022-12-15 16:15:07 -0500

    day 15

--- code/new.py
+++ code/new.py
@@ -1,10 +1,14 @@
-import ast
+# import ast
 
-# String representation of a list
-string = ""
+# # String representation of a list
+# string = ""
 
-# Convert the string to a list
-my_list = ast.literal_eval(string)
+# # Convert the string to a list
+# my_list = ast.literal_eval(string)
 
-# Print the list
-print(my_list)
+# # Print the list
+# print(my_list)
+
+
+for i in range(4000000 + 1):
+    print(i)
